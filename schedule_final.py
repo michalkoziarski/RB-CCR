ae2c53c2f6dbac27b354cdf9dcfd4ae7e96785af
michalkoziarski <michalkoziarski@gmail.com>
2020-06-02 17:40:08 +0200

    reordered final experiment

--- schedule_final.py
+++ schedule_final.py
@@ -1,13 +1,11 @@
-import datasets
 import os
 
 
 if __name__ == '__main__':
     for minority_training_size in [-1, 5, 10, 15, 20, 30]:
-        for dataset_name in datasets.names():
-            for fold in range(10):
-                for classifier_name in ['cart', 'knn', 'svm', 'lr', 'nb', 'mlp']:
-                        command = f'sbatch run.sh run_final.py -minority_training_size {minority_training_size} ' \
-                                  f'-dataset_name {dataset_name} -fold {fold} -classifier_name {classifier_name}'
+        for fold in range(10):
+            for classifier_name in ['cart', 'knn', 'svm', 'lr', 'nb', 'mlp']:
+                    command = f'sbatch run.sh run_final.py -minority_training_size {minority_training_size} ' \
+                              f'-fold {fold} -classifier_name {classifier_name}'
 
-                        os.system(command)
+                    os.system(command)
