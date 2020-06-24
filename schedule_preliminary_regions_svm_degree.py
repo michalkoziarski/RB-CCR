e2981cc051ee07c2e4a4f227c235a73a4aba2431
michal.koziarski <michal.koziarski@gmail.com>
2020-06-24 19:16:45 +0200

    modified token

--- schedule_preliminary_regions_svm_degree.py
+++ schedule_preliminary_regions_svm_degree.py
@@ -4,7 +4,7 @@ import os
 if __name__ == '__main__':
     for fold in range(10):
         for degree in range(1, 10):
-            classifier_name = f'svm({degree})'
+            classifier_name = f'svm[{degree}]'
 
             for regions in ['L', 'E', 'H', 'LEH']:
                 command = f'sbatch run.sh run_preliminary_regions_svm_degree.py ' \
