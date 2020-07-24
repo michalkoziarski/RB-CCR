4675a71c69413c21b1950ab2dfa2d84137d8cad9
michal.koziarski <michal.koziarski@gmail.com>
2020-07-24 18:44:55 +0200

    added multiclass metrics

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import imblearn.metrics
+import numpy as np
 import sklearn.metrics
 
 from collections import Counter
@@ -42,3 +43,99 @@ def specificity(ground_truth, predictions, majority_class=None):
         majority_class = Counter(ground_truth).most_common()[0][0]
 
     return sklearn.metrics.recall_score(ground_truth, predictions, pos_label=majority_class, zero_division=0)
+
+
+def confusion_matrix(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    if type(ground_truth) is not np.ndarray:
+        ground_truth = np.array(ground_truth)
+
+    if type(predictions) is not np.ndarray:
+        predictions = np.array(predictions)
+
+    n_classes = len(np.unique(ground_truth))
+    result = np.empty((n_classes,) * 2, np.int64)
+
+    for i, c1 in enumerate(np.unique(ground_truth)):
+        for j, c2 in enumerate(np.unique(ground_truth)):
+            result[i][j] = sum(((ground_truth == c1) & (predictions == c2)))
+
+    return result
+
+
+def class_accuracies(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    if type(ground_truth) is not np.ndarray:
+        ground_truth = np.array(ground_truth)
+
+    if type(predictions) is not np.ndarray:
+        predictions = np.array(predictions)
+
+    result = {}
+
+    for label in np.unique(ground_truth):
+        indices = (ground_truth == label)
+        result[label] = sklearn.metrics.accuracy_score(ground_truth[indices], predictions[indices])
+
+    return result
+
+
+def class_recalls(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    if type(ground_truth) is not np.ndarray:
+        ground_truth = np.array(ground_truth)
+
+    if type(predictions) is not np.ndarray:
+        predictions = np.array(predictions)
+
+    ground_truth = ground_truth.astype(np.int64)
+    predictions = predictions.astype(np.int64)
+
+    cm = np.array(confusion_matrix(ground_truth, predictions))
+    result = {}
+
+    for label, _ in enumerate(np.unique(ground_truth)):
+        result[label] = cm[label][label] / sum(cm[label])
+
+    return result
+
+
+def average_accuracy(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    return np.mean(list(class_accuracies(ground_truth, predictions).values()))
+
+
+def class_balance_accuracy(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    n_classes = len(np.unique(ground_truth))
+    cm = confusion_matrix(ground_truth, predictions)
+    result = 0
+
+    for i in range(n_classes):
+        sum_cm_i_j = sum([cm[i][j] for j in range(n_classes)])
+        sum_cm_j_i = sum([cm[j][i] for j in range(n_classes)])
+
+        result += cm[i][i] / max(sum_cm_i_j, sum_cm_j_i)
+
+    result /= n_classes
+
+    return result
+
+
+def geometric_average_of_recall(ground_truth, predictions):
+    assert len(ground_truth) == len(predictions)
+
+    recalls = class_recalls(ground_truth, predictions).values()
+    result = 1.0
+
+    for recall in recalls:
+        result *= recall
+
+    result = result ** (1 / len(recalls))
+
+    return result
