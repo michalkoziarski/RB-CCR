48bda81f7d99a22b5078a699d08e7675373715bd
michal.koziarski <michal.koziarski@gmail.com>
2020-08-09 16:37:17 +0200

    added script for merging results

---
+++ merge.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from pathlib import Path
+from tqdm import tqdm
+
+
+OUTPUT_DIRECTORY = Path('results')
+
+
+if __name__ == '__main__':
+    OUTPUT_DIRECTORY.mkdir(exist_ok=True, parents=True)
+
+    for trial in ['preliminary_energy', 'final']:
+        input_directory = Path(f'results_{trial}')
+
+        dfs = []
+
+        for path in tqdm(input_directory.iterdir(), desc=f'Trial: {trial}'):
+            df = pd.read_csv(path)
+            dfs.append(df)
+
+        df = pd.concat(dfs)
+        df.to_csv(OUTPUT_DIRECTORY / f'results_{trial}.csv', index=False)
