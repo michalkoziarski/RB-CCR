4c893614d9aadc37cfb1f24df95f4be52363ca1d
michal.koziarski <michal.koziarski@gmail.com>
2020-08-12 19:18:33 +0200

    changed gamma values used during visualization

--- visualize_potential.py
+++ visualize_potential.py
@@ -2,11 +2,11 @@ from visualization_tools import *
 
 
 if __name__ == '__main__':
-    dataset_name = 'yeast3'
+    dataset_name = 'pima'
 
-    X, y = prepare_data(dataset_name, scaler='Standard')
+    X, y = prepare_data(dataset_name, scaler='Standard', n_minority_samples=1000)
 
-    for gamma in [0.5, 0.75, 1.0, 1.25]:
+    for gamma in [0.1, 0.25, 0.5, 1.0]:
         visualize(
             X, y, gamma=gamma,
             file_name=f'potential_{gamma}.pdf'
